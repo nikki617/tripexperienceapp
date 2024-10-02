c6e5ddc5b3a9ecc56221ec4e8590e9d36871224f
nikki617 <nikki617@bu.edu>
2024-10-02 16:46:56 -0400

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from langchain.chains import RoutingChain
 from langchain_community import detect_sentiment
-!pip install streamlit langchain langchain_community
 import openai
 
 
